cf45b64ef01b6c663849d3006d33c52bb092f38a
U-Ifrit\jc <jcorkery@umich.edu>
2016-08-21 18:54:48 -0400

    Added proper folder context menu option for opening msys2 terminal on windows, removed lingering CMD when using shortcuts

--- assets/win/shortcut.bat
+++ assets/win/shortcut.bat
@@ -3,5 +3,11 @@ SETLOCAL ENABLEEXTENSIONS
 FOR /F "usebackq tokens=2,* skip=2" %%L IN (
     `REG QUERY "HKEY_CURRENT_USER\Software\Coebeef\Dynacoe" /ve`
 ) DO SET localPath=%%M
-C:\Windows\System32\cmd.exe /A /Q /K ""%localPath%\userspace\msys64\msys2_shell.cmd" "-mingw64" "-full-path""
+if [%1]==[] goto noPath
+C:\Windows\System32\cmd.exe /A /Q /C ""%localPath%\userspace\msys64\msys2_shell.cmd" "-mingw64" "-full-path" "-here""
+exit
+
+
+:noPath
+C:\Windows\System32\cmd.exe /A /Q /C ""%localPath%\userspace\msys64\msys2_shell.cmd" "-mingw64" "-full-path""
 exit

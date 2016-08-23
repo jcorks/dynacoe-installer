2e4057b6b1b74f13b956c4b230dc9e0ebbb485eb
U-Ifrit\jc <jcorkery@umich.edu>
2016-08-22 21:30:12 -0400

    Corrected  windows right-click shortcut to open in the clicked dir rather than the holding dir

--- assets/win/shortcut.bat
+++ assets/win/shortcut.bat
@@ -4,7 +4,7 @@ FOR /F "usebackq tokens=2,* skip=2" %%L IN (
     `REG QUERY "HKEY_CURRENT_USER\Software\Coebeef\Dynacoe" /ve`
 ) DO SET localPath=%%M
 if [%1]==[] goto noPath
-C:\Windows\System32\cmd.exe /A /Q /C ""%localPath%\userspace\msys64\msys2_shell.cmd" "-mingw64" "-full-path" "-here""
+C:\Windows\System32\cmd.exe /A /Q /C ""%localPath%\userspace\msys64\msys2_shell.cmd" "-mingw64" "-full-path" "-where" "%1""
 exit
 
 
